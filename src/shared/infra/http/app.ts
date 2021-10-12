573eb578fcc7b04b3d8d6d5e3c4bbca2b0549284
Vyctor <dev.vyctor@gmail.com>
2021-10-11 22:53:54 -0300

    feat: add express and AppError

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+
+import 'express-async-errors';
+import { AppError } from '../../errors/AppError';
+import createConnection from '../typeorm';
+
+createConnection();
+
+const app = express();
+
+app.use(express.json);
+
+app.use((err: Error, request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    response.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app };
