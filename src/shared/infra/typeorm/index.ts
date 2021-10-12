0a71a3b3ee0ee19cf9cd688a5803da4d58e614f8
Vyctor <dev.vyctor@gmail.com>
2021-10-11 23:28:07 -0300

    chore: add typeorm config do work with tdd

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,11 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-createConnection();
+export default async (host = 'api-ciclo-pagamentos-db'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database: process.env.NODE_ENV === 'test' ? 'ciclo-pagamentos-test' : defaultOptions.database,
+    }),
+  );
+};
