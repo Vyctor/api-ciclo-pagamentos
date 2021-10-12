8a703859210f6ab85c3ba781e1ae09aa868a7c9a
Vyctor <dev.vyctor@gmail.com>
2021-10-12 20:44:16 -0300

    refactor: remove billing cycle as a entity of database, is just a set of credits and debts

--- src/modules/billing-cycles/infra/typeorm/entities/BillingCycle.ts
+++ src/modules/billing-cycles/infra/typeorm/entities/BillingCycle.ts
@@ -1,33 +1,18 @@
-import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from 'typeorm';
-
 import { Credit } from '@modules/credits/infra/typeorm/entities/Credit';
 import { Debt } from '@modules/debts/infra/typeorm/entities/Debt';
 
-@Entity('billing-cycles')
 class BillingCycle {
-  @PrimaryColumn()
   public id: string;
 
-  @Column()
   public name: string;
 
-  @Column()
   public month: number;
 
-  @Column()
   public year: number;
 
-  @Column()
   public credits: Array<Credit>;
 
-  @Column()
   public debits: Array<Debt>;
-
-  @CreateDateColumn()
-  public created_at: Date;
-
-  @UpdateDateColumn()
-  public updated_at: Date;
 }
 
 export { BillingCycle };
