124346bac91c21fb7ca8703ce4e5647fb6930f0b
Vyctor <dev.vyctor@gmail.com>
2021-12-07 20:24:28 -0300

    feat: implementando controller ListAllCredits

---
+++ src/modules/credits/useCases/listAllCredits/ListAllCreditsController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { ListAllCreditsUseCase } from './ListAllCreditsUseCase';
+
+class ListAllCreditsController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const listAllCreditsUseCase = container.resolve(ListAllCreditsUseCase);
+
+    const credits = await listAllCreditsUseCase.execute();
+
+    return response.status(201).json(credits);
+  }
+}
+
+export default { ListAllCreditsController };
