cc2fbf466f61434e37b2fd87bd5f750e8efb2441
Vyctor <dev.vyctor@gmail.com>
2021-12-07 21:39:11 -0300

    feat: ajustando rota de debts

--- src/shared/infra/http/routes/debts.routes.ts
+++ src/shared/infra/http/routes/debts.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 
+import { CreateDebtController } from '@modules/debts/useCases/createDebt/CreateDebtController';
+import { DeleteDebtController } from '@modules/debts/useCases/deleteDebt/DeleteDebtController';
+import { ListAllDebtsController } from '@modules/debts/useCases/listAllDebts/ListAllDebtsController';
+import { ListDebtByIdController } from '@modules/debts/useCases/listDebtsById/ListDebtByIdController';
+
 const debtsRouter = Router();
 
-debtsRouter.post('/');
-debtsRouter.get('/');
-debtsRouter.patch('/');
-debtsRouter.delete('/');
+const createDebtController = new CreateDebtController();
+const listAllDebtController = new ListAllDebtsController();
+const listDebtByIdController = new ListDebtByIdController();
+const deleteDebtController = new DeleteDebtController();
+
+debtsRouter.post('/', createDebtController.handle);
+debtsRouter.get('/', listAllDebtController.handle);
+debtsRouter.get('/:id', listDebtByIdController.handle);
+debtsRouter.delete('/', deleteDebtController.handle);
 
 export default debtsRouter;
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import creditsRouter from './credits.routes';
+import debtsRouter from './debts.routes';
 
 const routes = Router();
 
 routes.use('/credits', creditsRouter);
+routes.use('/debts', debtsRouter);
 
 export default routes;
